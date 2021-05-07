345dfe250ca862b17cb1a8b5b753c02af290df02
maartendek <dekker.maarten@gmail.com>
2021-05-07 16:24:11 +0200

    add layer switch demo

---
+++ 3_base_layers/src/index.js
@@ -0,0 +1,72 @@
+import './scss/index.scss'
+
+import {Feature, Map, Overlay, View} from 'ol/index';
+import {OSM, XYZ, Vector as VectorSource} from 'ol/source';
+import {Point} from 'ol/geom';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {Circle, Fill, Stroke, Style} from 'ol/style';
+import LayerGroup from 'ol/layer/group';
+
+const place = [563836.2560250214, 6818208.299893484]; // Reykjavikplein 1
+
+const map = new Map({
+  target: "app",
+});
+
+const point = new Point(place);
+const features = [new Feature(point)];
+
+const featureLayer = new VectorLayer({
+  source: new VectorSource({
+    features,
+  }),
+  style: new Style({
+    image: new Circle({
+      radius: 9,
+      fill: new Fill({ color: "red" }),
+      stroke: new Stroke({ color: "black" })
+    }),
+  }),
+});
+
+const baseLayerOSM = new TileLayer({
+  source: new OSM(),
+  visible: false,
+  title: 'osm',
+});
+const baseLayerToner = new  TileLayer({
+  source: new XYZ({
+    url: 'http://b.tile.stamen.com/toner/{z}/{x}/{y}.png',
+    attributions: 'This belongs to stamen.com'
+  }),
+  visible: true,
+  title: 'toner',
+});
+const baseLayerGroup = new LayerGroup({
+  layers: [
+    baseLayerOSM,
+    baseLayerToner
+  ]
+});
+
+map.addLayer(baseLayerGroup);
+map.addLayer(featureLayer);
+
+const view = new View({
+  center: point.getCoordinates(),
+  zoom: 15,
+});
+map.setView(view);
+
+const setLayerVisible = (layerName) => {
+  baseLayerGroup.getLayers().forEach((el, index) => {
+    el.setVisible(el.get('title') === layerName)
+  })
+}
+
+// button behaviour
+document.querySelectorAll('#controls button').forEach(el => {
+  el.addEventListener("click", e => {
+    setLayerVisible(e.target.dataset.layer);
+  })
+})
